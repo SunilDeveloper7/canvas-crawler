a7d266cf2695f35f12cb67c0b9b06e36275aee2e
Sunil lama <Sunnil474@gmail.com>
2020-12-30 15:11:13 -0800

    Adding some characters

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log('🐸');
+//console.log('🐸');
 //reference to DOM ELEMENTS i will get
 //Hot off the press new way of doing things 
 
@@ -8,19 +8,63 @@ console.log('🐸');
 let movementDisplay = document.getElementById('movement')
 let game = document.getElementById('game')
 
-game.setAttribute('width', '800px')
-game.height = 400;
+game.setAttribute('width', '1000px')
+game.height = 700
 //Get some Context
 let ctx = game.getContext('2d')
+//get some context  
+let drawBox = (x,y,size,color) => {
+    ctx.fillStyle = color
+    ctx.fillRect(x,y,size,size)
+}
+
+//create some characters
+let ogre = {
+    x:30,
+    y:10,
+    color:'#bada55',
+    width:40,
+    height:80,
+    alive:true,
+    render:function() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x,this.y,this.width,this.height)
+
+    }
+}
+
+
+let hero = {
+    x:50,
+    y:100,
+    color:'hotpink',
+    width:60,
+    height:60,
+    alive:true,
+    render:function() {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x,this.y,this.width,this.height)
+    }
+}
+
+
+
+
+ogre.render()
+
+hero.render()
+//function render() {
+  //  ctx.fillStyle = 'red'
+    //ctx.fillRect (10,10,60,60)
 
 //Do Some drawing
 //fill color
-ctx.fillStyle = 'white'
+//ctx.fillStyle = 'white'
 //line color
-ctx.strokeStyle ='red'
+//ctx.strokeStyle ='red'
 
 //line width
-ctx.linewidth = 20
+//ctx.linewidth = 20
 
-ctx.fillRect(10,10,100,100);
-ctx.strokeRect(0,0,100,100)
+//ctx.fillRect(10,10,100,100);
+//ctx.strokeRect(0,0,100,100)
